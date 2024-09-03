d7963d9a6ff6897f11053d3b83552c733c363a89
hermannramos <140882889+hermannramos@users.noreply.github.com>
2024-09-03 10:44:09 +0000

    metodo put

--- src/front/js/component/ContactList.jsx
+++ src/front/js/component/ContactList.jsx
@@ -70,6 +70,31 @@ const ContactList = async () => {
 
    }
 
+   const editContacts = async (id) => {
+    const uri = host + `/contacts/${id}`;
+    const dataToSend = {
+        "name": `${contact}`,
+        "phone": "",
+        "email": "",
+        "address": ""
+      }
+    const options = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dataToSend)
+    }
+
+    const response = await fetch(uri, options);
+    if(!response.ok){
+        console.log(response.status);
+        return;
+    }
+    const data = response.json();
+    getContacts();
+   }
+
    useEffect(() => {
     getContacts()
    }, []);
@@ -77,34 +102,13 @@ const ContactList = async () => {
    return (
     <div className="container">
         <h1>Contact List</h1>
-        <button type="button" className="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Add New Contact</button>
-        <div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div className="modal-dialog">
-                <div className="modal-content">
-                    <div className="modal-header">
-                        <h1 className="modal-title fs-5" id="exampleModalLabel">New message</h1>
-                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div className="modal-body">
-                        <form>
-                        <div className="mb-3">
-                            <label for="recipient-name" className="col-form-label">Recipient:</label>
-                            <input type="text" className="form-control" id="recipient-name" />
-                        </div>
-                        <div className="mb-3">
-                            <label for="message-text" className="col-form-label">Message:</label>
-                            <textarea className="form-control" id="message-text"></textarea>
-                        </div>
-                        </form>
-                    </div>
-                    <div className="modal-footer">
-                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" className="btn btn-primary">Send message</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <ul></ul>
+        <ul>
+            {list.map((item) => (
+                <li key={item.id}>
+                    {item}
+                </li>
+            ))};
+        </ul>
     </div>
    );
 }
