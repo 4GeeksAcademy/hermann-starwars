5cd1d1904841d10afa7c8e467d66702ca93c9cd6
hermannramos <140882889+hermannramos@users.noreply.github.com>
2024-09-06 16:55:41 +0000

    styles get

--- src/front/js/component/ContactList.jsx
+++ src/front/js/component/ContactList.jsx
@@ -7,22 +7,18 @@ const ContactList = () => {
 
     return (
         <div className="container">
-            <h1>Contact List</h1>
-            <ul>
+            <h1>CONTACTS</h1>
+            <ul className='contact-list'>
                 {store.contacts.map((item) => (
                     <li key={item.id}>
-                    <div className="contact-info">
+                    <div className="contact-card">
                       <h2>{item.name}</h2>
                       <p><i className="fa fa-map-marker"></i> {item.address}</p>
                       <p><i className="fa fa-phone"></i> {item.phone}</p>
                       <p><i className="fa fa-envelope"></i> {item.email}</p>
                     </div>
-                    <div className="contact-actions">
-                      <button className="edit-btn"><i className="fa fa-pencil"></i></button>
-                      <button className="delete-btn"><i className="fa fa-trash"></i></button>
-                    </div>
                   </li>
-                ))};
+                ))}
             </ul>
         </div>
     );
