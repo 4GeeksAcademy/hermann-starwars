0ce571fe6f43e7a30944632907d84262d0dd25f3
hermannramos <140882889+hermannramos@users.noreply.github.com>
2024-09-12 18:57:29 +0000

    card style

--- src/front/js/component/characterList.jsx
+++ src/front/js/component/characterList.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { Context } from '../store/appContext';
 import { useNavigate } from 'react-router-dom';
+import darthvaderSlider from "../../img/darthvader-slider.jpg";
 
 const CharactersList = () => {
     const {store, actions} = useContext(Context);
@@ -23,13 +24,22 @@ const CharactersList = () => {
                         <div className="card">
                             <img src={`${store.host_starwars_imgs}/characters/${item.uid}.jpg`} className="card-img-top" alt={item.name} onError={handleError} style = {{aspectRatio:"3/4", width:"100%"}}/>
                             <div className="card-body bg-dark">
-                                <h2 className="card-title">{item.name}</h2>
+                                <h3 className="card-title">{item.name}</h3>
                                 <button type="reset" className="btn btn-warning me-2" onClick={() => {handleDetails(item.uid)}}>DETAILS</button>
                             </div>
                         </div>
                     </div>
-                ))}
+                    ))}
+                </div>
             </div>
+            <div className="wrapper">
+                <div className="card">
+                    <img src={darthvaderSlider} alt="" />
+                    <div className="info">
+                        <h3>Name Character</h3>
+                        <button type="reset" className="btn btn-warning me-2" onClick={() => {handleDetails(item.uid)}}>DETAILS</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
