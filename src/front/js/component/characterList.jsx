3df236a9570122a05b8a710dfd432254512a22b1
hermannramos <140882889+hermannramos@users.noreply.github.com>
2024-09-12 22:49:22 +0000

    list styles

--- src/front/js/component/characterList.jsx
+++ src/front/js/component/characterList.jsx
@@ -1,7 +1,6 @@
 import React, { useContext } from 'react';
 import { Context } from '../store/appContext';
 import { useNavigate } from 'react-router-dom';
-import darthvaderSlider from "../../img/darthvader-slider.jpg";
 
 const CharactersList = () => {
     const {store, actions} = useContext(Context);
@@ -20,11 +19,11 @@ const CharactersList = () => {
                 <h1>CHARACTERS</h1>
                 <div className="row">
                 {store.characters.map((item) => (
-                    <div key={item.uid} className="col-md-3 my-3">
+                    <div className="wrapper col-md-3 my-3" key={item.uid}>
                         <div className="card">
-                            <img src={`${store.host_starwars_imgs}/characters/${item.uid}.jpg`} className="card-img-top" alt={item.name} onError={handleError} style = {{aspectRatio:"3/4", width:"100%"}}/>
-                            <div className="card-body bg-dark">
-                                <h3 className="card-title">{item.name}</h3>
+                            <img src={`${store.host_starwars_imgs}/characters/${item.uid}.jpg`} alt={item.name} onError={handleError} />
+                            <div className="info">
+                                <h3>{item.name}</h3>
                                 <button type="reset" className="btn btn-warning me-2" onClick={() => {handleDetails(item.uid)}}>DETAILS</button>
                             </div>
                         </div>
@@ -32,17 +31,18 @@ const CharactersList = () => {
                     ))}
                 </div>
             </div>
-            <div className="wrapper">
-                <div className="card">
-                    <img src={darthvaderSlider} alt="" />
-                    <div className="info">
-                        <h3>Name Character</h3>
-                        <button type="reset" className="btn btn-warning me-2" onClick={() => {handleDetails(item.uid)}}>DETAILS</button>
-                    </div>
-                </div>
-            </div>
         </div>
     );
 }
 
 export default CharactersList;
+
+                /*<div key={item.uid} className="col-md-3 my-3">
+                        <div className="card">
+                            <img src={`${store.host_starwars_imgs}/characters/${item.uid}.jpg`} className="card-img-top" alt={item.name} onError={handleError} style = {{aspectRatio:"3/4", width:"100%"}}/>
+                            <div className="card-body bg-dark">
+                                <h3 className="card-title">{item.name}</h3>
+                                <button type="reset" className="btn btn-warning me-2" onClick={() => {handleDetails(item.uid)}}>DETAILS</button>
+                            </div>
+                        </div>
+                    </div> */
