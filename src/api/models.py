662381351fd47d3da6b7efe0bd84946cec943d92
hermannramos <140882889+hermannramos@users.noreply.github.com>
2024-09-25 19:30:55 +0000

    database

--- src/api/models.py
+++ src/api/models.py
@@ -7,8 +7,11 @@ db = SQLAlchemy()
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    password = db.Column(db.String(20), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    is_admin = db.Column(db.Boolean(), unique=False, nullable=False)
+    first_name = db.Column(db.String(), unique=False, nullable=True)
+    last_name = db.Column(db.String(), unique=False, nullable=True)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -18,3 +21,23 @@ class Users(db.Model):
         return {"id": self.id,
                 "email": self.email,
                 "is_active": self.is_active}
+    
+# Model Table
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, unique=False, nullable=False)
+    description = db.Column(db.String, unique=False, nullable=True)
+    body = db.Column(db.String, unique=False, nullable=False)
+    date = db.Column(db.DateTime, nullable=False) #Valor por defecto
+    image_url = db.Column(db.String)
+
+
+class Comments(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String, unique=False, nullable=False)
+
+
+class Medias(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    media_type = db.Column(db.Enum('image', 'video', 'podcast', name='media_type'))
+    url = db.Column(db.String)
