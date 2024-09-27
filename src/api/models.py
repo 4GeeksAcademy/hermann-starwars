297aa93631da2a058ef1d9b4f15eb1151124f83a
hermannramos <140882889+hermannramos@users.noreply.github.com>
2024-09-27 12:46:25 +0000

    tables done

--- src/api/models.py
+++ src/api/models.py
@@ -41,3 +41,34 @@ class Medias(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     media_type = db.Column(db.Enum('image', 'video', 'podcast', name='media_type'))
     url = db.Column(db.String)
+
+class Followers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+class CharacterFavorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+class PlanetFavorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+class Characters(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    height = db.Column(db.String, unique=False, nullable=True)
+    mass = db.Column(db.String, unique=False, nullable=True)
+    hair_color = db.Column(db.String, unique=False, nullable=True)
+    skin_color = db.Column(db.String, unique=False, nullable=True)
+    eye_color = db.Column(db.String, unique=False, nullable=True)
+    birth_year = db.Column(db.String, unique=False, nullable=True)
+    gender = db.Column(db.String, unique=False, nullable=True)
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    diameter = db.Column(db.String, unique=False, nullable=True)
+    rotation_period = db.Column(db.String, unique=False, nullable=True)
+    orbital_period = db.Column(db.String, unique=False, nullable=True)
+    gravity = db.Column(db.String, unique=False, nullable=True)
+    population = db.Column(db.String, unique=False, nullable=True)
+    climate = db.Column(db.String, unique=False, nullable=True)
+    terrain = db.Column(db.String, unique=False, nullable=True)
