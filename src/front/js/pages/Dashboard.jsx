bc3f5ffa284443012d0913330165b11466d40bd6
hermannramos <140882889+hermannramos@users.noreply.github.com>
2024-10-05 16:29:05 +0000

    dashboard

--- src/front/js/pages/Dashboard.jsx
+++ src/front/js/pages/Dashboard.jsx
@@ -11,7 +11,7 @@ const Dashboard = () => {
     return (
         <div className="container cont-space">
             <h1>Dashboard</h1>
-            <button className="btn btn-warning" onClick={handleOnClick} >Acceso a Protected</button>
+            <button className="btn btn-warning me-3" onClick={handleonClick} >Acceso a Protected</button>
             <button className="btn btn-success" onClick={() => actions.getPosts()} >Get Posts</button>
         </div>
     );
